645a7d91b76c817333a27d27cfdff778a67dcb26
ArunimaUniyal <59642538+ArunimaUniyal@users.noreply.github.com>
2020-01-18 20:10:17 +0530

    Update New.java

--- New.java
+++ New.java
@@ -4,7 +4,7 @@ public static void main(String args[])
 for(int i=0;i<=10;i++)
 {
 System.out.println("This is line no." +i);
-System.out.println("This is arunima");
+System.out.println("This is arunima and manish");
 }
 }
 }
